0bed8ba5ba0f547d72a853c948db70c42db88487
cristinafni <43381981+cristinafni@users.noreply.github.com>
2021-07-30 11:27:39 +0100

    Updated - to use for google collab

--- poserefine2_pytorch3d.py
+++ poserefine2_pytorch3d.py
@@ -9,6 +9,9 @@ This enables to create images that have the correct postion (ground truth)
 obtained from manual pose.
 It also allows to create blurred/noisy images based on ground truth.
 
+Using tutorial: https://pytorch3d.org/tutorials/camera_position_optimization_with_differentiable_rendering
+
+
 @author: Cristina
 """
 
@@ -19,7 +22,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import logging
-
+from skimage.color import rgb2gray
 from stl.mesh import Mesh
 
 from pytorch3d.structures import Meshes, join_meshes_as_batch
@@ -37,25 +40,22 @@ from pytorch3d.renderer import (
     SoftSilhouetteShader,
     HardPhongShader,
 )
-
 import imageio
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from skimage import img_as_ubyte
-
 # 3D transformations functions
 from pytorch3d.transforms import Rotate, Translate
-
 # rendering components
 from pytorch3d.renderer import (
     FoVPerspectiveCameras, look_at_view_transform, look_at_rotation, 
     RasterizationSettings, MeshRenderer, MeshRasterizer, BlendParams,
     SoftSilhouetteShader, HardPhongShader, PointLights, TexturesVertex,
 )
-
 from scipy.ndimage.filters import gaussian_filter
 from rotation import rotation_matrix_from_euler
+from losses import JaccardLoss
 
 class Pytorch3dViewer():
     """creates render images from stls and poses that can be backpropagated"""
@@ -69,9 +69,9 @@ class Pytorch3dViewer():
 
         # standard values from `deepautomatch.viewer`
         # as we are only using calibration-corrected poses here, these are the important ones
-        std_img_size = 1000
-        std_cal_mm_per_pxl = 0.29
-        std_cal_focal_length = 970
+        std_img_size = 1000 # change?
+        std_cal_mm_per_pxl = 0.29 
+        std_cal_focal_length = 972
 
         fov_angle = np.degrees(np.arctan(std_img_size * std_cal_mm_per_pxl / std_cal_focal_length))
         # for render images
@@ -106,7 +106,7 @@ class Pytorch3dViewer():
         self.silhouette_renderer = MeshRenderer(
             rasterizer=MeshRasterizer(cameras=cameras, raster_settings=raster_settings),
             shader=SoftSilhouetteShader(blend_params=blend_params),
-        )
+        ) # parameters to change!!!
 
     def mesh_from_stl(self, stl):
         # Load the stl and ignore the textures and materials.
@@ -121,7 +121,6 @@ class Pytorch3dViewer():
         return implant_mesh
     
     def join_meshes(self, femur_stl, tibia_stl):
-        
         # Load the stl and ignore the textures and materials.
         femur_stl = Mesh.from_file(Path(femur_stl))
         femur_verts = torch.from_numpy(np.reshape(femur_stl.vectors, (-1, 3)))
@@ -147,7 +146,6 @@ class Pytorch3dViewer():
         faces = torch.cat((femur_verts, tibia_verts), axis=0)
         verts_rgb = torch.cat((femur_verts_rgb, tibia_verts_rgb), axis=1)
         textures = TexturesVertex(verts_features= torch.Tensor(verts_rgb))
-        
         
         implant_mesh_joined = Meshes(verts=[verts.to(self.device)], faces=[faces.to(self.device)], textures=textures.to(self.device))
         return implant_mesh_joined
@@ -252,7 +250,7 @@ class Pytorch3dViewer():
             meshes.append(self.mesh_from_stl(stl))
 
         meshes = join_meshes_as_batch(meshes)
-
+        
         # adjust for differences in coordinate conventions
         m = torch.Tensor([[-1, 0, 0], [0, 1, 0], [0, 0, -1]]).to(R.device)
         T = torch.matmul(T, m)
@@ -263,9 +261,9 @@ class Pytorch3dViewer():
             meshes_world=meshes, R=R.to(self.device), T=T.to(self.device)
         )
         # get rid of alpha channel
-        imgs = imgs[..., :3]
+        imgs1 = imgs[..., :3]
         
-        return imgs
+        return imgs1
     
     def scene_snapshot_shadow_one_component(
         self,
@@ -284,14 +282,12 @@ class Pytorch3dViewer():
         img = img / img.max() 
         return img
 
-    
 def create_ground_truth(index):
     device = torch.device("cpu")
     viewer = Pytorch3dViewer(200, device=device)
     d = Path("./data_files/stl_3D_models")
     femur_stls = [d / "cr_fem_4_r_narrow_mm.stl"]
     tibia_stls = [d / "cr_tib_modular_3_r_narrow.stl"]
-
     # create list 
     data_folder = "./data_files/csv_files/flumatch_data.csv"
     f = open(data_folder)
@@ -318,7 +314,6 @@ def create_ground_truth(index):
     
         for k, v in poses.items():
             poses[k] = torch.Tensor(v)
-        
         mm_per_pxl = 0.2876
         femur_R = rotation_matrix_from_euler(
             poses["femur_rx"], poses["femur_ry"], poses["femur_rz"]
@@ -339,11 +334,9 @@ def create_ground_truth(index):
         tibia_T = torch.cat(
             [torch.unsqueeze(v, dim=0) for v in [poses["tibia_tx"], poses["tibia_ty"], poses["tibia_tz"]]]
         ).T
-    
         py3d_imgs_shadow = viewer.scene_snapshot_shadow(
             femur_stls, tibia_stls, femur_R, femur_T, tibia_R, tibia_T
         )
-        
         py3d_imgs_shadow = py3d_imgs_shadow.cpu().numpy()
         reference_list.append(py3d_imgs_shadow[0].squeeze())
     
@@ -378,7 +371,6 @@ def create_ground_truth_one_component(index, component ='tibia'):
             for k, v in poses.items():
                 poses[k] = torch.Tensor(v)
             
-            
             mm_per_pxl = 0.2876
             
             tibia_R = rotation_matrix_from_euler(
@@ -394,11 +386,8 @@ def create_ground_truth_one_component(index, component ='tibia'):
             tibia_T = torch.cat(
                 [torch.unsqueeze(v, dim=0) for v in [poses["tibia_tx"], poses["tibia_ty"], poses["tibia_tz"]]]
             ).T
-        
             py3d_imgs_shadow = viewer.scene_snapshot_shadow_one_component(tibia_stls, tibia_R, tibia_T)
-            
             py3d_imgs_shadow = py3d_imgs_shadow.cpu().numpy()
-            
             reference_list.append(py3d_imgs_shadow[0].squeeze())
         
     elif component == 'femur':
@@ -417,19 +406,14 @@ def create_ground_truth_one_component(index, component ='tibia'):
                 poses[k] = torch.Tensor(v)
                 
             mm_per_pxl = 0.2876
-            
             femur_R = rotation_matrix_from_euler(
                 poses["femur_rx"], poses["femur_ry"], poses["femur_rz"]
             )
-            
             femur_T = torch.cat(
                 [torch.unsqueeze(v, dim=0) for v in [poses["femur_tx"], poses["femur_ty"], poses["femur_tz"]]]
             ).T
-            
             py3d_imgs_shadow = viewer.scene_snapshot_shadow_one_component(femur_stls, femur_R, femur_T)
-            
             py3d_imgs_shadow = py3d_imgs_shadow.cpu().numpy()
-            
             reference_list.append(py3d_imgs_shadow[0].squeeze())
     
     return reference_list 
@@ -441,26 +425,23 @@ def create_blurred_shadows(index):
     for a in reference_list:
         blurred = gaussian_filter(a, sigma=0.5)
         blurred_list.append(blurred)
-        
     return blurred_list
 
-
 class Model(nn.Module):
-    def __init__(self, meshes, renderer, image_ref, device=None):
+    def __init__(self, meshes, renderer, phong_renderer, image_ref, device=None):
         super().__init__()
         if device is None:
             self.device = torch.device("cuda:0")
         else:
             self.device = torch.device("cpu")
         self.meshes = meshes
-    
-        # Get the silhouette of the reference RGB image by finding all non-white pixel values. 
-        image_ref = torch.from_numpy((image_ref[..., :3].max(-1) != 1).astype(np.float32))
-        self.register_buffer('image_ref', image_ref)
+        self.renderer = renderer
+        self.phong_renderer = phong_renderer
+        # self.image_ref = image_ref
         
-        # Create an optimizable parameter for R, T position 
-        # self.camera_position = nn.Parameter(
-        #     torch.from_numpy(np.array([5.7698, 5.7876, 972.7146], dtype=np.float32)).to(device)) 
+        #Get the silhouette of the reference image by finding all non-black pixel values. 
+        # image_ref = torch.from_numpy((image_ref[..., :3].max(-1) != 1).astype(np.float32))
+        # self.register_buffer('image_ref', image_ref)
         
         data_folder = "./data_files/csv_files/flumatch_data.csv"
         f = open(data_folder)
@@ -471,47 +452,73 @@ class Model(nn.Module):
                 "femur_rx": [load_data_info['femur_rx'][0]],
                 "femur_ry": [load_data_info['femur_ry'][0]],
                 "femur_rz": [load_data_info['femur_rz'][0]],
-                "femur_tx": [load_data_info['femur_tx'][0]+2],
-                "femur_ty": [load_data_info['femur_ty'][0]+2],
-                "femur_tz": [load_data_info['femur_tz'][0]+2],
+                "femur_tx": [load_data_info['femur_tx'][0]+5],
+                "femur_ty": [load_data_info['femur_ty'][0]+5],
+                "femur_tz": [load_data_info['femur_tz'][0]+5],
                 }
             
         for k, v in poses.items():
-            poses[k] = torch.Tensor(v)
-            
-        mm_per_pxl = 0.2876
-        
-        R = rotation_matrix_from_euler(
-            poses["femur_rx"], poses["femur_ry"], poses["femur_rz"]
-        )
-        
-        T = torch.cat(
+            poses[k] = torch.Tensor(v) 
+        #mm_per_pxl = 0.2876
+        self.R = rotation_matrix_from_euler(
+            poses["femur_rx"], poses["femur_ry"], poses["femur_rz"])
+        self.T = torch.cat(
             [torch.unsqueeze(v, dim=0) for v in [poses["femur_tx"], poses["femur_ty"], poses["femur_tz"]]]
+        ).T
+
+        self.m = torch.Tensor([[-1, 0, 0], [0, 1, 0], [0, 0, -1]]).to(self.R.device)
+        self.T = torch.matmul(self.T, self.m)
+        self.R = self.R.permute(0, 2, 1)
+        self.R = torch.matmul(self.R, self.m)
+       
+        # Parameters to optimise
+        self.R = nn.Parameter(self.R.to(self.device))
+        self.T = nn.Parameter(self.T.to(self.device))
+        
+        # Reference 
+        
+        poses_ref = {
+             "femur_rx": [load_data_info['femur_rx'][0]],
+             "femur_ry": [load_data_info['femur_ry'][0]],
+             "femur_rz": [load_data_info['femur_rz'][0]],
+             "femur_tx": [load_data_info['femur_tx'][0]],
+             "femur_ty": [load_data_info['femur_ty'][0]],
+             "femur_tz": [load_data_info['femur_tz'][0]],
+             }
+            
+        for k, v in poses_ref.items():
+            poses_ref[k] = torch.Tensor(v) 
+        #mm_per_pxl = 0.2876
+        self.R_ref = rotation_matrix_from_euler(
+            poses_ref["femur_rx"], poses_ref["femur_ry"], poses_ref["femur_rz"])
+        self.T_ref = torch.cat(
+            [torch.unsqueeze(v, dim=0) for v in [poses_ref["femur_tx"], poses_ref["femur_ty"], poses_ref["femur_tz"]]]
         ).T
         
         # py3d_imgs_shadow = viewer.scene_snapshot_shadow_one_component(femur_stls, femur_R, femur_T)
         # py3d_imgs_shadow = py3d_imgs_shadow.cpu().numpy()
-        
-        self.R = nn.Parameter(R=R.to(self.device))
-        self.T = nn.Parameter(T=T.to(self.device))
-        
-        #self.phong_renderer = nn.Parameter()
-
+        self.m_ref = torch.Tensor([[-1, 0, 0], [0, 1, 0], [0, 0, -1]]).to(self.R_ref.device)
+        self.T_ref = torch.matmul(self.T_ref, self.m_ref)
+        self.R_ref = self.R_ref.permute(0, 2, 1)
+        self.R_ref = torch.matmul(self.R_ref, self.m_ref)
+        self.image_ref = self.renderer(meshes_world=self.meshes, R=self.R_ref, T=self.T_ref)
+    
     def forward(self):
-        # Render the image using the updated camera position. Based on the new position of the 
-        # camera we calculate the rotation and translation matrices
-        # R = look_at_rotation(self.camera_position[None, :], device=self.device)  # (1, 3, 3)
-        # T = -torch.bmm(R.transpose(1, 2), self.camera_position[None, :, None])[:, :, 0]   # (1, 3)
-        # image = self.renderer(meshes_world=self.meshes.clone(), R=R, T=T)
-        # create list 
+
+        # print(type(self.offset_R))
+        image1 = self.renderer(meshes_world=self.meshes, R=self.R, T=self.T)
+        # image1 = image1.detach().squeeze().cpu().numpy()
+        # image1[image1!=0] = 1
+        image_ref = self.image_ref
         
-        T = -torch.bmm(R.transpose(1, 2))
-        image = self.phong_renderer(meshes_world=self.meshes.clone(), self.R==R, self.T==T)
-    
         # Calculate the silhouette loss
-        loss = torch.sum((image[..., 3] - self.image_ref) ** 2)
-        return loss, image
-    
+        loss_j = JaccardLoss()
+        loss = loss_j.forward(pred = image1, target = self.image_ref)
+        
+        # loss = torch.sum((image1 - self.image_ref) ** 2)
+        # loss = torch.sum((image1[...,3] - self.image_ref) ** 2)
+        
+        return loss, image1, image_ref
     
 def rescale_array(image):
     rescale = 2.*(image - np.min(image))/np.ptp(image)-1
@@ -522,91 +529,79 @@ def optimisation():
     device = torch.device("cpu")
     # blurred_list = create_blurred_shadows(index=1)
     reference_list =  create_ground_truth_one_component(index=1, component ='femur')
-    
     d = Path("./data_files/stl_3D_models")
     femur_stls = [d / "cr_fem_4_r_narrow_mm.stl"]
     tibia_stls = [d / "cr_tib_modular_3_r_narrow.stl"]
     viewer = Pytorch3dViewer(200, device=device)
     
     #for b in blurred_list:
-    for b in reference_list:
-      
+    for ref_img in reference_list:
         plt.figure()
         plt.grid(False)
-        plt.imshow(b, cmap="gray")
+        plt.imshow(ref_img)
        
         # # Initialize a model using the renderer, mesh and reference image
-        model1 = Model(viewer.mesh_from_stl(femur_stls[0]),viewer.silhouette_renderer, b, device=device)
-        # # model2 = Model(viewer.mesh_from_stl(tibia_stls[0]),viewer.silhouette_renderer, b, device=device)
-        # # model = Model(viewer.join_meshes(femur_stls[0], tibia_stls[0]),viewer.silhouette_renderer, b, device=device)
-       
-        # # model1 = Model(viewer.scene_snapshot(femur_stls[0], tibia_stls[0], femur_R, femur_T, tibia_R, tibia_T), b, device=device)
-        # # Create an optimizer. Here we are using Adam and we pass in the parameters of the model
-        # optimizer = torch.optim.Adam(model1.parameters(), lr=0.05)
+        ref_img[ref_img!=1] = 0
+        model1 = Model(viewer.mesh_from_stl(femur_stls[0]),viewer.silhouette_renderer, viewer.phong_renderer, ref_img, device=device)
+        optimizer = torch.optim.Adam(model1.parameters(), lr=0.05) #change?
         
-        # plt.figure(figsize=(10, 10))
-
-        _, image_init = model1()
-        plt.subplot(1, 3, 1)
-        plt.imshow(image_init.detach().squeeze().cpu().numpy()[..., 3])
+        plt.figure(figsize=(10, 10))
+        _, image_init, image_ref = model1()
+        image_start = image_init.detach().squeeze().cpu().numpy()[..., 3]
+        image_start[image_start!=0] = 1
+       
+        plt.subplot(1, 2, 1)
+        # plt.imshow(image_init, cmap="gray")
+        plt.imshow(image_start, cmap="gray")
         plt.grid(False)
         plt.title("Starting position")
         
-        # # _, image_init = model2()
-        # # plt.subplot(1, 3, 2)
-        # # plt.imshow(image_init.detach().squeeze().cpu().numpy()[..., 3])
-        # # plt.grid(False)
-        # # plt.title("Starting position")
+        image_new_ref = model1.image_ref
+        image_new_ref = image_new_ref.detach().squeeze().cpu().numpy()[..., 3]
+        image_new_ref[image_new_ref!=0] = 1
         
-        plt.subplot(1, 3, 3)
-        plt.imshow(b, cmap="gray")
+        plt.subplot(1, 2, 2)
+        plt.imshow(image_new_ref, cmap="gray")
         plt.grid(False)
         plt.title("Reference silhouette");
        
-        # for i in range(200):
-        #     print(i)
-        #     optimizer.zero_grad()
-        #     loss, _ = model1()
-        #     loss.backward()
-        #     optimizer.step()
-        #     if loss.item() < 200: 
-        #         break
-        #     if i % 10 == 0:
-        #         R = look_at_rotation(model1.camera_position[None, :], device=model1.device)
-        #         T = -torch.bmm(R.transpose(1, 2), model1.camera_position[None, :, None])[:, :, 0]   # (1, 3)
-        #         image = viewer.phong_renderer(meshes_world=model1.meshes.clone(), R=R, T=T)
-        #         image = image[0, ..., :3].detach().squeeze().cpu().numpy()
-        #         print(type(image))
-        #         image = rescale_array(image)
-        #         # image = img_as_ubyte(image)
-        #         #writer.append_data(image)
-        #         plt.figure()
-        #         plt.imshow(image[..., :3])
-        #         plt.title("iter: %d, loss: %0.2f" % (i, loss.data))
-        #         plt.grid("off")
-        #         plt.axis("off")
-
+        # Optimisation loop
+        loss_list = []
+        iterations = range(1000)
+        
+        for i in iterations: # change number
+            print(i)
+            optimizer.zero_grad()
+            loss, _, _ = model1()  
+            loss.backward()
+            optimizer.step()
+            
+            # if loss.item() < 100: 
+            #     break
+            # if i % 10 == 0:
+            image = viewer.phong_renderer(meshes_world=model1.meshes.clone(), R=model1.R, T=model1.T)
+            image = image[0, ..., :3].detach().squeeze().cpu().numpy()
+            # image = rescale_array(image)
+            image[image!=0] = 1
+            # image = img_as_ubyte(image)
+            loss_list.append(loss.data)
+            
+        plt.figure()
+        plt.imshow(rgb2gray(image), cmap='gray')
+        plt.title("iter: %d, loss: %0.2f" % (i, loss.data))
+        plt.grid("off")
+        plt.axis("off")
+            
+        plt.figure()
+        plt.plot(iterations, loss_list)
+        plt.xlabel('Iterations')
+        plt.ylabel('Jaccard Loss')
+        plt.title("Loss VS iterations")
+       
 
 def main():
     device = torch.device("cpu")
     optimisation()
-     # reference_list = create_ground_truth(index=3)
-     
-     # for a in reference_list:
-       
-     #    plt.figure()
-     #    plt.grid(False)
-     #    plt.imshow(a, cmap="gray")
-     
-     # blurred_list = create_blurred_shadows(index=1)
-     # viewer = Pytorch3dViewer(200, device=device)
-     # for b in blurred_list:
-       
-     #    plt.figure()
-     #    plt.grid(False)
-     #    plt.imshow(b, cmap="gray")
-        
-     #    model_create = Model(viewer.mesh_from_stl,viewer.silhouette_renderer, b, device=device)
      
 
 if __name__ == "__main__":
